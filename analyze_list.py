808fc674e4d574d709e195b096aec67a05fc1f08
Emily Meschke <emilymeschke@guest-wireless-upc-1605-10-120-086-191.usc.edu>
2018-07-30 12:40:38 -0700

    added example trial file

--- analyze_list.py
+++ analyze_list.py
@@ -4,8 +4,8 @@ from funcs_evaluate import stats
 from funcs_evaluate import list_stats
 
 # INPUTS
-in_file = 'googles_all.csv'
-list_file = 'hmf_list_v1.csv'
+in_file = 'all_data.csv'
+list_file = 'hmf_list_ex.csv'
 N = 9320
 # number of bins
 n_bins = 10
@@ -15,7 +15,7 @@ n_per_bin = N/n_bins
 n_test = int(n_per_bin*(.10))
 
 
-# print stats 
+# print stats for all celebs
 print stats(in_file, n_bins, n_per_bin)
-# analyze list
+# analyze sampled list generated
 print list_stats(list_file, n_test)
--- sort_list.py
+++ sort_list.py
@@ -1,10 +1,65 @@
+# MERGE SORT IMPLEMENTATION TO SORT LIST FROM HIGHEST TO LOWEST 
+# NUMBER OF GOOGLE SEARCH RESULTS
+
 import csv
 
-unsorted_list = [['' for x in range(0,5)] for y in range(0,9450)]
-with open('all_data.csv') as csvin:
-	reader = csv.reader(csvin)
-	for row in reader:
-		unsorted_list = row
+# Read in file
+def read_file(in_file):
+	unsorted_list = []
+	with open('all_data.csv') as csvin:
+		reader = csv.reader(csvin)
+		for row in reader:
+			unsorted_list.append(row)
+	return unsorted_list
 
-for ul in unsorted_list:
-	print ul
+# Write to file
+def write_file(out_file, listt):
+	n = len(out_file)
+	with open(out_file, 'wb') as csvout:
+		writer = csv.writer(csvout)
+		i = 0
+		for i in range(0,n):
+		    writer.writerow(listt[i])
+		    i = i+1
+
+def merge(left, right):
+	res = []
+	while (len(left) > 0) and (len(right) > 0):
+		if (left[0] > right[0]):
+			res.append(left[0])
+			left.pop(0)
+		else:
+			res.append(right[0])
+			right.pop(0)
+
+	while (len(left) > 0):
+		res.append(left[0])
+		left.pop(0)
+	while (len(right) > 0):
+		res.append(right[0])
+		right.pop(0)
+
+	return res
+
+def merge_sort(m):
+	n = len(m)
+	if (n <= 1):
+		return m
+
+	mid = n//2
+	left = m[:mid]
+	right = m[mid+1:]
+
+	left = merge_sort(left)
+	right = merge_sort(right)
+
+	return merge(left, right)
+
+
+unsorted_list = read_file('all_data.csv')
+merge_sort(unsorted_list)
+
+write_file('all_data_ordered.csv', unsorted_lis)
+
+
+
